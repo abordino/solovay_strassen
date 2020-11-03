31809bcfd05a84f04cbbc5c6795a0e80d54a57ff
Luca <Bordino>
2020-11-03 11:56:48 +0100

    Add error handling  for main

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
     try:
         n = int(input("Which positive integer do you want to test? "))
         assert n > 1
-    except:
+    except ValueError:
+        print("Not a valid integer, an error occurred casting the input")
+    except AssertionError:
         print("Integer must be >= 2")
     else:
         if solov.test_solovay(n):
